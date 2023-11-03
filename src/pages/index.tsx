629bdcb1e770e0ce381366c98c3cd041bc4e932a
Pawan Senpura [@Rootcode] <134264455+pawan-rtc@users.noreply.github.com>
2023-11-03 17:19:56 +0530

    Develop (#9)
    * fix: test
    
    * chore(release): 1.1.1-alpha.2 [skip ci]
    
    ## [1.1.1-alpha.2](https://github.com/pawan-rtc/semantic-versioning-test/compare/v1.1.1-alpha.1...v1.1.1-alpha.2) (2023-11-03)
    
    ### Bug Fixes
    
    * test ([231c5b9](https://github.com/pawan-rtc/semantic-versioning-test/commit/231c5b92a33b18299f438340e5601e760a997e85))
    
    * chore(release): 1.1.2-alpha.1 [skip ci]
    
    ## [1.1.2-alpha.1](https://github.com/pawan-rtc/semantic-versioning-test/compare/v1.1.1...v1.1.2-alpha.1) (2023-11-03)
    
    ### Bug Fixes
    
    * test ([231c5b9](https://github.com/pawan-rtc/semantic-versioning-test/commit/231c5b92a33b18299f438340e5601e760a997e85))
    
    * fix: add new line to index js
    
    * fix: add a 2nd new line
    
    * chore(release): 1.1.2-alpha.2 [skip ci]
    
    ## [1.1.2-alpha.2](https://github.com/pawan-rtc/semantic-versioning-test/compare/v1.1.2-alpha.1...v1.1.2-alpha.2) (2023-11-03)
    
    ### Bug Fixes
    
    * add a 2nd new line ([63c8a89](https://github.com/pawan-rtc/semantic-versioning-test/commit/63c8a8947b6624097ed32dedbd006f0b957c026e))
    * add new line to index js ([cf9ca51](https://github.com/pawan-rtc/semantic-versioning-test/commit/cf9ca51698fdf7520f225341d87b908e4bdd80ef))
    
    * fix: fix 3
    
    * fix: fix 4
    
    * fix: fix 5
    
    * chore(release): 1.1.2-alpha.3 [skip ci]
    
    ## [1.1.2-alpha.3](https://github.com/pawan-rtc/semantic-versioning-test/compare/v1.1.2-alpha.2...v1.1.2-alpha.3) (2023-11-03)
    
    ### Bug Fixes
    
    * fix 3 ([a44c630](https://github.com/pawan-rtc/semantic-versioning-test/commit/a44c630f4e6d3fe960714286871a062bc2dd4d78))
    * fix 4 ([804de65](https://github.com/pawan-rtc/semantic-versioning-test/commit/804de6573eb4a2ea70cee05e869ca3d3717c12af))
    * fix 5 ([ba29799](https://github.com/pawan-rtc/semantic-versioning-test/commit/ba297997b196ab8d41f3ae8a9559e4e1579cd312))
    
    * feat: newline fix 6
    
    * feat: newline fix 7
    
    * chore(release): 1.2.0-alpha.1 [skip ci]
    
    # [1.2.0-alpha.1](https://github.com/pawan-rtc/semantic-versioning-test/compare/v1.1.2-alpha.3...v1.2.0-alpha.1) (2023-11-03)
    
    ### Features
    
    * newline fix 6 ([672788e](https://github.com/pawan-rtc/semantic-versioning-test/commit/672788ebdc66f7c20d10d8efd0c8b651c707f704))
    * newline fix 7 ([a04607b](https://github.com/pawan-rtc/semantic-versioning-test/commit/a04607b393bbd56e8ba87cf11c818b7e0eaa9dfd))
    
    * fix: newline fix 8
    
    * fix: newline fix 9
    
    * feat: newline feat 10
    
    * chore(release): 1.2.0-alpha.2 [skip ci]
    
    # [1.2.0-alpha.2](https://github.com/pawan-rtc/semantic-versioning-test/compare/v1.2.0-alpha.1...v1.2.0-alpha.2) (2023-11-03)
    
    ### Bug Fixes
    
    * newline fix 8 ([118fbb8](https://github.com/pawan-rtc/semantic-versioning-test/commit/118fbb89ca0be96f6e8a834921f275fc01c4e74f))
    * newline fix 9 ([4770d03](https://github.com/pawan-rtc/semantic-versioning-test/commit/4770d0332235cf716691f994c15aff4fcec3ffa6))
    
    ### Features
    
    * newline feat 10 ([fbf7bd8](https://github.com/pawan-rtc/semantic-versioning-test/commit/fbf7bd8392347a1666d82f5edb7a2e96dd64f7c8))
    
    * feat: newline feat 11
    
    * chore(release): 1.2.0-alpha.3 [skip ci]
    
    # [1.2.0-alpha.3](https://github.com/pawan-rtc/semantic-versioning-test/compare/v1.2.0-alpha.2...v1.2.0-alpha.3) (2023-11-03)
    
    ### Features
    
    * newline feat 11 ([36073f8](https://github.com/pawan-rtc/semantic-versioning-test/commit/36073f894d74df617921ee1a58e14035a9fb8ebb))
    
    * feat: newline 12
    
    * feat: newline 13
    
    * chore(release): 1.2.0-alpha.4 [skip ci]
    
    # [1.2.0-alpha.4](https://github.com/pawan-rtc/semantic-versioning-test/compare/v1.2.0-alpha.3...v1.2.0-alpha.4) (2023-11-03)
    
    ### Features
    
    * newline 12 ([a7c83f7](https://github.com/pawan-rtc/semantic-versioning-test/commit/a7c83f72ef6619fda8f78b93cbb61e695191e0ae))
    * newline 13 ([8f6d63b](https://github.com/pawan-rtc/semantic-versioning-test/commit/8f6d63b24b57fa4eeba0ad7ca3ffaa59d8d1f653))
    
    ---------
    
    Co-authored-by: pawan-live <pawan.senpura@gmail.com>
    Co-authored-by: semantic-release-bot <semantic-release-bot@martynus.net>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,6 +13,24 @@ export default function Home() {
         <h1>This is a new feature 3</h1>
 
         <h1>This is a new feature on DEVELOP</h1>
+        <h1>This is a new feature on DEVELOP 4</h1>
+        <h1>This is a new feature on TEST-10 Branch (Fix)</h1>
+        <h1>This is a new feature on TEST-10 Branch (Fix 2)</h1>
+        <h1>This is a new feature on TEST-11 Branch (Fix 3)</h1>
+        <h1>This is a new feature on TEST-11 Branch (Fix 4)</h1>
+        <h1>This is a new feature on TEST-11 Branch (Fix 5)</h1>
+
+        <h1>This is a new FEAT on TEST-12 Branch (Fix 6)</h1>
+        <h1>This is a new FEAT on TEST-12 Branch (Fix 7)</h1>
+
+        <h1>This is a new FIX on TEST-13 Branch (Fix 8)</h1>
+        <h1>This is a new FIX on TEST-13 Branch (Fix 9)</h1>
+        <h1>This is a new FEAT on TEST-13 Branch (Feat 10)</h1>
+
+        <h1>This is a new FEAT on TEST-14 Branch (Feat 11)</h1>
+
+        <h1>This is a new FEAT on TEST-15 Branch (Feat 12)</h1>
+        <h1>This is a new FEAT on TEST-15 Branch (Feat 13)</h1>
       </div>
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
